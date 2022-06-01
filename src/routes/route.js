a9bd3aadb14a68226657ebf6880ed54a445f2417
DonIndra95 <royindrashish95@gmail.com>
2022-06-01 15:57:00 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
 const router = express.Router();
+const logger = require('./logger')
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+
+    logger.log()
+    res.send(logger.url)
+    res.send('My first ever api! and above is written in another file')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('Hello')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('World')
+});
+router.get('/test-me4', function (req, res) {
+    res.send('Hi')
 });
 
 module.exports = router;
