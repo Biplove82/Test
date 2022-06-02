19c0989df301196b5c0630525ab3e992a9aacabf
DonIndra95 <royindrashish95@gmail.com>
2022-06-02 14:14:58 +0530

    External module implemented

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash= require('lodash');
 
 const router = express.Router();
 const logger = require('../logger/logger.js')
@@ -14,6 +15,22 @@ router.get('/test-me', function (req, res) {
     formatter.lower()
     formatter.upper()
     res.send('My first ever api!')
+});
+
+router.get('/hello', function (req, res) {
+const months=['January','February','March','April','May','June','July','August','September','October','November','December'];
+const chunked=lodash.chunk(months,3);
+console.log(chunked);
+const odd=[];
+for(let i=0;i<20;i++){
+    if(i % 2 !== 0)
+    odd.push(i);
+}
+const tailed= lodash.tail(odd)
+console.log(tailed)
+const unioned= lodash.union([2,43,32],[4,2,54,10],[32,43,56],[12,45],[99])
+console.log(unioned)    
+    res.send('My Hello api!')
 });
 
 module.exports = router;
