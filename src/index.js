ddcf2caaedc2c5b3e65b23d3d800af0328eec853
DonIndra95 <royindrashish95@gmail.com>
2022-06-10 22:11:22 +0530

    Assignment on Global Middleware

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment= require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://IndrashishRoy:windows10@radon-cohort-cluster.gtmdsvp.mongodb.net/Indrashish95-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -16,7 +17,10 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        const date=moment().format('DD-MM-YYYY, HH:mm:ss');
+        const ipAddress= req.ip;
+        const r=req.originalUrl
+        console.log (date," , ",ipAddress," , ",r);
         next();
   }
   );
