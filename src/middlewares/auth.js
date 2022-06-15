2fcc4681c30b1e38d8ba36b314796c23079f4ad3
DonIndra95 <royindrashish95@gmail.com>
2022-06-15 21:12:40 +0530

    Callback function to remove error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,20 +5,18 @@ const authentication = async function (req, res,next) {
     if (!token) token = req.headers["x-auth-token"];
   
     //If no token is present in the request header return error
-    if (!token) return res.send({ status: false, msg: "token must be present" });
-  
-    console.log(token);
-    
-    // If a token is present then decode the token with verify function
-    // verify takes two inputs:
-    // Input 1 is the token to be decoded
-    // Input 2 is the same secret with which the token was generated
-    // Check the value of the decoded token yourself
-    let decodedToken = jwt.verify(token, "functionup-radon");
-    if (!decodedToken){
-      return res.send({ status: false, msg: "Token is invalid" });
-    }
-    else {next()}
+    if (!token) return res.send({ status: false, msg: "token must be present" });  
+    let decodedToken = jwt.verify(token, "functionup-radon",function(err,data){
+      if(err){
+        return res.send({ status: false, msg: "Token is invalid" });
+      }else {
+         next()
+      }
+    });
+    // if (!decodedToken){
+    //   return res.send({ status: false, msg: "Token is invalid" });
+    // }
+    // else {next()}
 }
 const authorization=async function (req, res,next) {
   let token = req.headers["x-Auth-token"];
